d842da5c6e66bcc6bd2028a5fefe0bf4d6aba661
Shakib Idrishi <shakibidrishi17@gmail.com>
2025-10-11 20:19:15 +0530

    index add

--- src/index.js
+++ src/index.js
@@ -13,9 +13,8 @@ const PORT = process.env.PORT || 3000;
 job.start();
 app.use(express.json()); //midlerwaere
 app.use(cors()); //midlerware
-
 app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 
 
 app.use("/api/auth", authRoutes);
